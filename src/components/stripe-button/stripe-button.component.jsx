6af7841c4e76da6fc322663c6e12f59d6bda3a70
Marek S <mareks@live.com>
2020-07-03 19:31:19 +0300

    updating stripe integration, favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51H0rSxGMJd6L032cIL4iiiPFzylYmxVSWJ23RxmCuPW567GiI0SkvdwZwD1GhlGqdSUYBLO7Z32f97rxAidutpmm00bZH52mgp";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing"
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
